36f7ad5c75e36f9822e10f0e6c7b904af8e1b605
Arinobu Okamoto <kamoarito@gmail.com>
2021-06-30 11:39:23 -0500

    Se cambio el tipo de conexion

--- include/conexion.php
+++ include/conexion.php
@@ -1,13 +1,28 @@
 <?php
   function OpenCon(){
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "root";
-    $db = "harina";
-    // $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+    // $dbhost = "localhost";
+    // $dbuser = "root";
+    // $dbpass = "root";
+    // $db = "harina";
+    // // $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+    // $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
 
-    return $conn;
+
+    $user ="root";
+    $password="root";
+    $host="localhost";
+    $port="3307";
+    $db="harina";
+    $link = mysqli_init();
+    $con = mysqli_real_connect($link,$host,$user,$password,$db,$port);
+
+    if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    $con=mysqli_connect($host,$user,$password,$db,$port);
+
+    return $con;
   }
 
   function CloseCon($conn){
