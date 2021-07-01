94633d2be8fcee05a6210fc8cc654e12b225cb02
Arinobu Okamoto <kamoarito@gmail.com>
2021-06-30 23:08:27 -0500

    Se ajustaron cuestiones de diseño y se regreso a la forma antigua de conexion por cuestiones raras

--- almacenista/pdf.php
+++ almacenista/pdf.php
@@ -1,8 +1,21 @@
 <?php
 require('fpdf/fpdf.php');
 
-include("../include/conexion.php");
-$con = OpenCon();
+$user ="root";
+$password="root";
+$host="localhost";
+$port="3307";
+$db="harina";
+$link = mysqli_init();
+$conn = mysqli_real_connect($link,$host,$user,$password,$db,$port);
+
+if (mysqli_connect_errno()) {
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$con=mysqli_connect($host,$user,$password,$db,$port);
+
+
 
 class PDF extends FPDF
 {
