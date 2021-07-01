0fd6d38d16ca3400421dabcaf036f46d769e95e9
Arinobu Okamoto <kamoarito@gmail.com>
2021-06-30 22:54:35 -0500

    Ajustes en la manera de conexion a la bd

--- include/conexion.php
+++ include/conexion.php
@@ -1,31 +1,23 @@
 <?php
   function OpenCon(){
-    // $dbhost = "localhost";
-    // $dbuser = "root";
-    // $dbpass = "root";
-    // $db = "harina";
-    // // $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-    // $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
-
-
     $user ="root";
     $password="root";
     $host="localhost";
     $port="3307";
     $db="harina";
     $link = mysqli_init();
-    $con = mysqli_real_connect($link,$host,$user,$password,$db,$port);
+    $conn = mysqli_real_connect($link,$host,$user,$password,$db,$port);
 
     if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-
+    
     $con=mysqli_connect($host,$user,$password,$db,$port);
 
     return $con;
   }
 
-  function CloseCon($conn){
-    $conn -> close();
+  function CloseCon($con){
+    $con -> close();
   }
 ?>
