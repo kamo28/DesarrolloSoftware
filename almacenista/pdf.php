1b9a3c41359977cc95233e291dd31deea88ac58d
Arinobu Okamoto <kamoarito@gmail.com>
2021-06-30 22:39:31 -0500

    Prueba

--- almacenista/pdf.php
+++ almacenista/pdf.php
@@ -2,20 +2,6 @@
 <?php
 require('fpdf/fpdf.php');
 
-// $user ="root";
-// $password="root";
-// $host="localhost";
-// $port="3307";
-// $db="harina";
-// $link = mysqli_init();
-// $conn = mysqli_real_connect($link,$host,$user,$password,$db,$port);
-
-// if (mysqli_connect_errno()) {
-// echo "Failed to connect to MySQL: " . mysqli_connect_error();
-// }
-
-// $con=mysqli_connect($host,$user,$password,$db,$port);
-
 include("../include/conexion.php");
 $con = OpenCon();
 
